32a3e1b76c97bc4813511da68459c80a2b2e4c89
hossemedineali <hossemedine@outlook.com>
2023-03-24 03:45:47 +0100

    added agents

--- src/components/new/step2.tsx
+++ src/components/new/step2.tsx
@@ -1,7 +1,12 @@
 import { useChoises, useModal } from "@/store/newStore";
 
+import man from '../../../public/man.png'
+import woman from '../../../public/woman.jpg'
+import blacnk from '../../../public/blank.webp'
+import Image from "next/image";
 
 
+const agents=[{name:'Any agent ',pic:blacnk},{name:'Michael',pic:man},{name:'Katelyn',pic:woman},{name:'Brandan',pic:man},{name:'Thomas',pic:man},{name:'Gloria',pic:woman}]
 
 const Step2 = () => {
     const {toggleOpen}=useModal()
@@ -11,6 +16,16 @@ const Step2 = () => {
                 <div className=" flex justify-between p-2 items-center text-xl border-b">
                         <p className=" text-blue-900  font-medium text-center text-xl ">Select Agent</p>
                         <span onClick={toggleOpen} className="text-2xl hover:cursor-pointer ">X</span>
+                </div>
+
+                <div className="grid grid-cols-3 justify-center items-center p-2">
+                        {agents.map(({name,pic},idx)=>{
+                            return <div key={ idx} className='group hover:cursor-pointer hover:border-blue-900 transition-all duration-150 ease-linear border mt-3 w-28 aspect-square flex flex-col justify-center items-center '>
+                                <Image src={pic.src} width={64} height={64} alt='agent' className="rounded-full group-hover:scale-75 transition-all duration-150 ease-linear group-hover:-translate-y-2 "/> 
+                                <p className=" w-full group-hover:-translate-y-3 transition-all duration-150 ease-linear  text-lg text-center">{name}</p>
+                                <p className="text-xs hidden group-hover:block group-hover:-translate-y-3 transition-transform duration-150 ease-linear underline decoration-dotted underline-offset-4 decoration-blue-900 hover:decoration-red-800">View details</p>
+                                </div>
+                        })}
                 </div>
 
 
